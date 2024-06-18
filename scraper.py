94e9392609fc2ba8662d812286f779fde6dad8ba
BenMc123123 <mcdonaldben02@gmail.com>
2024-06-18 18:26:16 +0100

    Prototype working ish
    Can read the table, change number of entries, just need to itterate through stuff now

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 
 
@@ -38,7 +38,7 @@ username_field_2.send_keys(personal_username)
 password_field_2.send_keys(personal_password)
 
 # submit 
-login_button = driver.find_element(By.NAME, "btnSubmit")
+login_button = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.NAME, "btnSubmit")))
 login_button.click()
 
 # go to events page
@@ -51,7 +51,48 @@ driver.execute_script("document.getElementsByName('ctl00$ctl00$cphBaseBody$cphBo
 driver.execute_script("document.getElementsByName('ctl00$ctl00$cphBaseBody$cphBody$cbDraft')[0].click();")
 driver.execute_script("document.getElementsByName('ctl00$ctl00$cphBaseBody$cphBody$cbPending')[0].click();")
 driver.execute_script("document.getElementsByName('ctl00$ctl00$cphBaseBody$cphBody$btnFilter')[0].click();")
+driver.execute_script("document.getElementsByName('ctl00$ctl00$cphBaseBody$cphBody$cbToggleDisplay')[0].click();")
 
 
+
+select_entries = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.NAME, 'eventTable_length')))
+select = Select(select_entries)
+# this changes the table to show all entries/events
+select.select_by_value('-1')
+
+time.sleep(5)
+numberOf = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'eventTable_info')))
+numberOf_string = numberOf.text
+numberOf_split = numberOf_string.split(" ")
+numberOfEvents = numberOf_split[5]
+
+link = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_cphBaseBody_cphBody_lvEventDetails_ctrl0_lbAttendees')))
+link.click()
+
+print(1)
+div = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "modal-content")))
+# div = driver.find_element(by=By.CLASS_NAME, value="modal-content")
+table = div.find_elements(by=By.XPATH, value='//*/tbody')
+
+rows = table[1].find_elements(by=By.TAG_NAME, value="tr")
+
+# Column to choose by its index, say the second column in the table
+desired_column = 1
+desired_column_data = []
+
+for row in rows:
+    columns = row.find_elements(by=By.TAG_NAME, value='td')
+
+    for index, col in enumerate(columns):
+        if index == desired_column:
+            desired_column_data.append(col.text)
+
+print(desired_column_data)
+
+# closes list
+# link = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_cphBaseBody_cphBody_eventNoticeboard_btnCloseModal')))
+# link.click()
+# print(2)
+
+# ctl00_ctl00_cphBaseBody_cphBody_lvEventDetails_ctrl7_lbAttendees
 time.sleep(500)
-
