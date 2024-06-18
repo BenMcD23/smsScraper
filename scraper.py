0153e72ebac2d7dd1a3b7b45689ed8297ce76ffa
BenMc123123 <mcdonaldben02@gmail.com>
2024-06-18 22:10:16 +0100

    Works kind of now, goes through all events, just need to format well

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.common.exceptions import ElementClickInterceptedException, StaleElementReferenceException, TimeoutException
 
 import time
 # Set up the WebDriver (make sure chromedriver is in your PATH)
@@ -60,39 +60,79 @@ select = Select(select_entries)
 # this changes the table to show all entries/events
 select.select_by_value('-1')
 
-time.sleep(5)
 numberOf = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'eventTable_info')))
 numberOf_string = numberOf.text
 numberOf_split = numberOf_string.split(" ")
-numberOfEvents = numberOf_split[5]
+numberOfEvents = int(numberOf_split[5])
 
-link = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_cphBaseBody_cphBody_lvEventDetails_ctrl0_lbAttendees')))
-link.click()
-
-print(1)
-div = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "modal-content")))
-# div = driver.find_element(by=By.CLASS_NAME, value="modal-content")
-table = div.find_elements(by=By.XPATH, value='//*/tbody')
-
-rows = table[1].find_elements(by=By.TAG_NAME, value="tr")
-
-# Column to choose by its index, say the second column in the table
-desired_column = 1
-desired_column_data = []
-
+# get name of each event
+table = driver.find_elements(by=By.XPATH, value='//*/tbody')
+rows = table[0].find_elements(by=By.TAG_NAME, value="tr")
+event_names = []
 for row in rows:
     columns = row.find_elements(by=By.TAG_NAME, value='td')
-
     for index, col in enumerate(columns):
-        if index == desired_column:
-            desired_column_data.append(col.text)
+        if index == 1:
+            event_names.append((col.text).replace("\n", " "))
+print(event_names)
+data = []
 
-print(desired_column_data)
+for i in range(numberOfEvents):
+    try:
+        # click an individual event
+        link = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_cphBaseBody_cphBody_lvEventDetails_ctrl' + str(i) +'_lbAttendees')))
+        link.click()
+        
+        try:
+            # this changes the table to show all entries/events
+            select_entries = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.NAME, 'ctl00_ctl00_cphBaseBody_cphBody_eventNoticeboard_gvCadetsAttendees_length')))
+            select = Select(select_entries)
+            select.select_by_value('-1')
+        except TimeoutException as e:
+            print("Probably only staff on event")
 
-# closes list
-# link = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_cphBaseBody_cphBody_eventNoticeboard_btnCloseModal')))
-# link.click()
-# print(2)
+        # get the table of names
+        div = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "modal-content")))
+        table = div.find_elements(by=By.XPATH, value='//*/tbody')
+        rows = table[1].find_elements(by=By.TAG_NAME, value="tr")
 
-# ctl00_ctl00_cphBaseBody_cphBody_lvEventDetails_ctrl7_lbAttendees
+        # Column to choose by its index, say the second column in the table
+        each_event = []
+        for row in rows:
+            columns = row.find_elements(by=By.TAG_NAME, value='td')
+            each_row = []
+            for col in columns:
+                each_row.append(col.text)
+            each_event.append(each_row)
+        
+        data.append(each_event)
+            # for index, col in enumerate(columns):
+            #     # print(col.text)
+            #     if index == desired_column:
+            #         desired_column_data.append(col.text)
+
+
+        
+        # closes list
+        div = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "modal-footer")))
+        link = div.find_element(by=By.ID, value='ctl00_ctl00_cphBaseBody_cphBody_eventNoticeboard_btnCloseModal')
+        
+        # link = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_cphBaseBody_cphBody_eventNoticeboard_btnCloseModal')))
+        link.click()
+        # print(2)
+
+        time.sleep(1)
+    except ElementClickInterceptedException as e:
+        print("No cadets on event")
+    except StaleElementReferenceException as e:
+        print("Increase sleep time")
+    # except TimeoutException as e:
+    #     print("Probably only staff on event")
+    #     # close list
+    #     div = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "modal-footer")))
+    #     link = div.find_element(by=By.ID, value='ctl00_ctl00_cphBaseBody_cphBody_eventNoticeboard_btnCloseModal')
+
+print(data)
+
+print("end")
 time.sleep(500)
